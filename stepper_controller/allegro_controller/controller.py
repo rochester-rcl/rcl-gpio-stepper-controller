a6746ea68f711b540f4a918f14686b6c04e0fc6e
jromphf <jromphf@library.rochester.edu>
2018-04-16 12:59:28 -0400

    cleaned up the code and added a better thread-based "move" method

--- stepper_controller/allegro_controller/controller.py
+++ stepper_controller/allegro_controller/controller.py
@@ -1,9 +1,8 @@
 # import RPi.GPIO as GPIO
-from threading import Thread
+from threading import Thread, Lock
 from enum import Enum
 import time
 from default_stepper.defaults import default_allegro
-
 
 class AllegroControls:
     MICROSTEP_FULL = 0
@@ -11,6 +10,9 @@ class AllegroControls:
     MICROSTEP_QUARTER = 2
     MICROSTEP_EIGHTH = 3
     MICROSTEP_SIXTEENTH = 4
+    DEFAULT_STEP_ANGLE = 1.8
+    DEFAULT_RPM = 180
+    _lock = Lock()
     """
     A class to control an Allegro stepper driver carrier like the `Pololu A4988 <https://www.pololu.com/product/1182>`_
     """
@@ -24,12 +26,27 @@ class AllegroControls:
                 <stepper_controller.default_stepper.html#module-stepper_controller.default_stepper.defaults>`_
                 You can replace any / all of the following keys:
                 dir | step | enable | vdd | microstep - ms1 | ms2 | ms3
+            step_angle (``float``) -- 
+                The step angle of the motor. Default is 1.8 degrees
+            rpm (``int``) -- 
+                The desired speed of the motor in revolutions per minute. Default is 60.
         """
         if 'stepper' in kwargs:
             self.stepper = {**kwargs['stepper'], **default_allegro()}
         else:
             self.stepper = default_allegro()
 
+        if 'step_angle' in kwargs:
+            self.step_angle = kwargs['step_angle']
+        else:
+            self.step_angle = self.DEFAULT_STEP_ANGLE
+
+        if 'rpm' in kwargs:
+            self._rpm = kwargs['rpm']
+        else:
+            self._rpm = self.DEFAULT_RPM
+
+        self.steps_per_rev = int(360  / self.step_angle)
         self.mode = GPIO.setmode(GPIO.BCM)
         self.motor_stopped = False
         self.paused = False
@@ -47,7 +64,7 @@ class AllegroControls:
 
         # stop motors w/ enable pins
         GPIO.output(self.stepper['enable'], GPIO.HIGH)
-        self._thread = Thread(target=self._forward, args=())
+        self._thread = Thread(target=self._move, args=())
 
     def _set_microstep_resolution(self, pins, pin_modes):
 
@@ -55,6 +72,20 @@ class AllegroControls:
         for pin, mode in zip(pins, pin_modes):
             print(pin, mode)
             set_microstep(pin[1], mode)
+
+    @property
+    def rps(self):
+        """
+        The speed of the motor in revolutions per second 
+        """
+        return int(self._rpm / 60)
+
+    @property
+    def rpm(self):
+        """
+        The speed of the motor in revolutions per minute
+        """
+        return int(self._rpm)
 
     def microstep(self, resolution):
         """
@@ -104,29 +135,53 @@ class AllegroControls:
         GPIO.output(self.stepper['dir'], state)
         GPIO.output(self.stepper['enable'], state)
 
-    def motor_forward(self):
+    def set_speed(self, rpm):
         """
-        Moves the motor forward in a separate thread
+        :param rpm (``int``): The speed of the motor in revolutions per minute 
+        """
+        with self._lock:
+            self._rpm = rpm
+
+    def move(self):
+        """
+        Continuously moves the motor in the current direction (executes in a separate thread)
         """
         self._thread.start()
 
-    def _forward(self):
+    def _move(self):
+        sleep_duration = int(self.rps / self.steps_per_rev)
+        while True:
+            for i in range(0, self.steps_per_rev):
+                with self._lock:
+                    if self.motor_stopped is True:
+                        self._motor_shutdown()
+                        break
+                    if self.paused:
+                        self._pause()
+                        break
+                    self.step()
+                    time.sleep(sleep_duration)
+
+    def step(self):
+        """
+        Moves the motor one step in the current direction
+        """
         GPIO.output(self.stepper['step'], GPIO.HIGH)
         GPIO.output(self.stepper['step'], GPIO.LOW)
 
-        if self.motor_stopped:
-            self.motor_shutdown()
-
-        if self.paused:
-            self.pause()
+    def _pause(self):
+        GPIO.output(self.stepper['step'], GPIO.LOW)
 
     def pause(self):
         """
         Pauses the motor's movement
         """
-        GPIO.output(self.stepper['step'], GPIO.LOW)
+        self.paused = not self.paused
 
     def _motor_shutdown(self):
+        """
+        Shuts down the motor and does any necessary GPIO cleanup
+        """
         GPIO.output(self.stepper['enable'], GPIO.HIGH)
         for key, pin in self.stepper.items():
             if key is 'microstep':
@@ -153,9 +208,9 @@ class AllegroControls:
 if __name__ == '__main__':
     controls = AllegroControls()
     controls.microstep(AllegroControls.MICROSTEP_FULL)
-    controls.motor_setup()
+    controls.motor_setup(True)
     for step in range(0, 100):
         time.sleep(0.004)
-        controls.motor_forward()
+        controls.step()
     controls.disable()
     controls.close()
